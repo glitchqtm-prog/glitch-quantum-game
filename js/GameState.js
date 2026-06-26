cbd8d4844c19d4c2eb809c54c9dbf93db5e8692f
glitchqtm-prog <glitchqtm@gmail.com>
2026-06-26 12:36:17 +0300

    Update GameState.js

--- js/GameState.js
+++ js/GameState.js
@@ -1,1 +1,25 @@
+class GameState {
+    constructor() {
+        this.data = this.load() || {
+            stats: { str: 10, agi: 5, int: 5, luck: 2 },
+            resources: { gqt: 0, energy: 100 },
+            level: 1,
+            inventory: []
+        };
+    }
 
+    save() {
+        // Veriyi basit bir şekilde kaydediyoruz
+        localStorage.setItem('glitch_quantum_save', JSON.stringify(this.data));
+    }
+
+    load() {
+        const saved = localStorage.getItem('glitch_quantum_save');
+        return saved ? JSON.parse(saved) : null;
+    }
+
+    updateStats(newStats) {
+        this.data.stats = { ...this.data.stats, ...newStats };
+        this.save();
+    }
+}
