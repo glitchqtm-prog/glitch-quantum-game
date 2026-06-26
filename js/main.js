2b33ff1ba1e41bbef87dcdae092b609d476aa530
glitchqtm-prog <glitchqtm@gmail.com>
2026-06-26 12:35:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+const state = new GameState();
+const combat = new CombatEngine(state);
 
+function gameLoop() {
+    // 1. Düşmanları güncelle
+    // 2. Karakter animasyonunu oynat
+    // 3. Ekrana çizdir
+    requestAnimationFrame(gameLoop);
+}
+
+// Oyunu başlat
+gameLoop();
