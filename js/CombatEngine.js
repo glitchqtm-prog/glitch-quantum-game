44e27dc07fe9325280ae1311d800ddc7e70613ad
glitchqtm-prog <glitchqtm@gmail.com>
2026-06-26 12:57:04 +0300

    Update CombatEngine.js

--- js/CombatEngine.js
+++ js/CombatEngine.js
@@ -1,15 +1,14 @@
 class CombatEngine {
     constructor(state) {
         this.state = state;
-        this.currentEnemy = { hp: 100 };
+        this.enemyHp = 100;
     }
-
     attack() {
-        let dmg = this.state.data.stats.str * 2;
-        this.currentEnemy.hp -= dmg;
-        if (this.currentEnemy.hp <= 0) {
-            this.currentEnemy.hp = 100;
-            this.state.data.resources.gqt += 10;
+        let dmg = this.state.data.stats.str;
+        this.enemyHp -= dmg;
+        if (this.enemyHp <= 0) {
+            this.enemyHp = 100 + (this.state.data.level * 10);
+            this.state.data.resources.gqt += 5;
             this.state.save();
         }
         return dmg;
